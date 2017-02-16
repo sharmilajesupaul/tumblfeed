250ea33b0df6e1ae542f811adfc72e60d9def652
Sharmila Jesupaul <tosharmila@gmail.com>
2017-02-15 19:56:00 -0800

    instantiates vue

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # tumblfeed
+
+Run `npm i` and then  `npm start` to run the app on port 3000.
+
--- public/script.js
+++ public/script.js
@@ -1,16 +1,10 @@
-var xhr = new XMLHttpRequest();
-var data = {};
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState == XMLHttpRequest.DONE) {
-        try {
-          data = JSON.parse(xhr.response);
-          init(data);
-        } catch (err) {
-          console.error("Unable to parse data");
-        }
-    }
+var Card = {
+  template: document.getElementById('card').innerHTML
 };
-xhr.open('GET', '/data', true);
-xhr.send(null);
 
+new Vue({
+  el: '#root-wrapper',
+  components: {
+    'card': Card
+  }
+});
